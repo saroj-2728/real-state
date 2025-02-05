4c43e1671b12da6dad123586fb6a756229306737
SAROJ <080bct075.saroj@pcampus.edu.np>
2025-02-05 21:39:42 +0545

    initiated frontend with react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Dashboard from './components/dashboard/dashboard'
+// import Navbar from './components/navbar'
+
+function App() {
+
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,37 @@
+import '../styles/navbar.css';
+import { useState } from 'react';
+
+const Navbar = () => {
+
+    const [tab, setTab] = useState("Buy");
+
+    return (
+        <nav>
+            <div className="logo">
+                <img
+                    src="/images/logo.png" alt="Logo"
+                    className='logo-img'
+                />
+                <p className='logo-text'>ORIGIN HOMES</p>
+            </div>
+
+            <div className="tabs">
+                <ul>
+                    <li>Buy</li>
+                    <li>Sell</li>
+                    <li>Rent</li>
+                    <li>Rent Out</li>
+                </ul>
+                <div>
+                    <input type="text" placeholder="Search..." />
+                    <button>Search</button>
+                </div>
+            </div>
+
+            
+
+        </nav>
+    )
+}
+
+export default Navbar
